cd90712fa21e10124f7ca91e8c471beaa19d7a49
malinachitic24-star <malinachitic24@gmail.com>
2026-03-22 12:34:05 +0100

    Borrar item

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //1. Importar los módulos NPM que necesito instalación
 const express = require("express");
 const cors = require("cors");
+const methodOverride = require("method-override");
 
 //5. Conectar MySQL y ExpressJS
 const mysql = require("mysql2/promise");
@@ -34,6 +35,7 @@ app.set("view engine", "ejs");
 app.use(cors()); //da permiso para que lo nombren desde un servidor externo(localhost:5173 de React)
 app.use(express.json()); //leer datos que te manda el frontend en json
 app.use(express.static(path.join(__dirname, "../public"))); //construccion correcta de archivos
+app.use(methodOverride("_method"));
 
 //4. Arrancar el servidor en el puerto
 const serverPort = 3000;
@@ -47,97 +49,108 @@ app.listen(serverPort, () => {
 
 app.get("/products", async (req, res) => {
   try {
-  const connection = await getConnection(); //crea conección 
-  const sql = `SELECT * FROM products`; //constante mysql para solicitar ver tabla
-  const [results] = await connection.query(sql);  //coge el primero y lo iguala a la constante anterior 
-  connection.end(); // cierra la conexión
-  res.json(results); //lo convierte en json 
-  }
-  catch (error) {
-    console.error (error);
-    res.status(500).json ({error: "Error al obtener productos"});
+    const connection = await getConnection(); //crea conección
+    const sql = `SELECT * FROM products`; //constante mysql para solicitar ver tabla
+    const [results] = await connection.query(sql); //coge el primero y lo iguala a la constante anterior
+    connection.end(); // cierra la conexión
+    res.json(results); //lo convierte en json
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener productos" });
   }
 });
 
 app.post("/products", async (req, res) => {
   try {
-  const connection = await getConnection();
-  const { name, price, category, stock } = req.body; //recibe datos front objeto
-  const sql = `INSERT INTO products (name, price, category, stock) VALUES (?,?,?,?)`; //Insert BD
+    const connection = await getConnection();
+    const { name, price, category, stock } = req.body; //recibe datos front objeto
+    const sql = `INSERT INTO products (name, price, category, stock) VALUES (?,?,?,?)`; //Insert BD
 
-  const [results] = await connection.query(sql, [name, price, category, stock]); //guarda en result
+    const [results] = await connection.query(sql, [
+      name,
+      price,
+      category,
+      stock,
+    ]); //guarda en result
 
-  connection.end();
+    connection.end();
 
-  res.json({
-    success: true,
-    id: results.insertId,
-  });
-} 
-catch (error) {
-  console.error(error);
-  res.status(500).json({error: "Error al crea producto"})
-}
+    res.json({
+      success: true,
+      id: results.insertId,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al crea producto" });
+  }
 });
 
 app.get("/cart", async (req, res) => {
   try {
-  const connection = await getConnection();
-  const sql = `SELECT * FROM cart`;
-  const [results] = await connection.query(sql);
-  connection.end();
-  res.json(results);
-  }
-  catch (error) {
+    const connection = await getConnection();
+    const sql = `SELECT * FROM cart`;
+    const [results] = await connection.query(sql);
+    connection.end();
+    res.json(results);
+  } catch (error) {
     console.error(error);
-    res.status(500).json({error: "Error al obtener carrito"})
+    res.status(500).json({ error: "Error al obtener carrito" });
   }
 });
 
 app.post("/cart", async (req, res) => {
   try {
-  const connection = await getConnection();
-  const { quantity, product_id } = req.body;
+    const connection = await getConnection();
+    const { quantity, product_id } = req.body;
 
-  const sql = `
+    const sql = `
     INSERT INTO cart (quantity, product_id)
     VALUES (?, ?)
   `;
 
-  const [results] = await connection.query(sql, [quantity, product_id]);
+    const [results] = await connection.query(sql, [quantity, product_id]);
 
-  connection.end();
+    connection.end();
 
-  res.json({
-    success: true,
-    id: results.insertId,
-  });
-}
-  catch (error) {
+    res.json({
+      success: true,
+      id: results.insertId,
+    });
+  } catch (error) {
     console.error(error);
-    res.status(500).json({error: "Error al insertar datos de carrito"})
+    res.status(500).json({ error: "Error al insertar datos de carrito" });
   }
 });
 
 app.get("/cart-view", async (req, res) => {
   try {
-  const connection = await getConnection();
-  // primero const sql = `SELECT * FROM cart` -> pero tenía que cambiar los datos
-  const sql = `SELECT products.name, cart.quantity, products.price FROM cart JOIN products ON cart.product_id = products.idproducts`;
-  const [results] = await connection.query(sql);
+    const connection = await getConnection();
+    // primero const sql = `SELECT * FROM cart` -> pero tenía que cambiar los datos
+    const sql = `SELECT cart.idcart, products.name, cart.quantity, products.price FROM cart JOIN products ON cart.product_id = products.idproducts`;
+    const [results] = await connection.query(sql);
 
-  //Cambiar los valóres numéricos a números porque siempre lo cogen en sting
-  results.forEach((item) => {
-    item.price = Number(item.price);
-  });
+    //Cambiar los valóres numéricos a números porque siempre lo cogen en sting
+    results.forEach((item) => {
+      item.price = Number(item.price);
+    });
+
+    connection.end();
+    console.log(results);
+    res.render("cart", { cart: results });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al nostrar carrito" });
+  }
+});
+
+app.delete("/cart/:id", async (req, res) => {
+  const connection = await getConnection();
+
+  const { id } = req.params;
+
+  await connection.query("DELETE FROM cart WHERE idcart = ?", [id]);
 
   connection.end();
-  console.log(results);
-  res.render("cart", { cart: results });
-}
-  catch (error) {
-    console.error(error);
-    res.status(500).json({error: "Error al nostrar carrito"})
-    
-  }
+
+  res.json({success: true});
 });
